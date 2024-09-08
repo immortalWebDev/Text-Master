43ed8e57779d96dadafe3c415fd9fbc65bc0f7a6
immortalWebDev <pgbadgujar007@gmail.com>
2024-09-08 16:07:09 +0530

    Added Footer for app

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>
+                Created with ❤ by Piyush
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
