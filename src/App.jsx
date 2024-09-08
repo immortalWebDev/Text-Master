ae85f7ce185a3ed0f5aed2ecdd0baa1f30d8dfbb
immortalWebDev <pgbadgujar007@gmail.com>
2024-09-08 16:07:58 +0530

    Passed footer in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import { Box } from "@mui/system";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./components/AboutUs";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<AboutUs />} />
         </Routes>
       </Box>
+      <Footer/>
     </Router>
   );
 }
