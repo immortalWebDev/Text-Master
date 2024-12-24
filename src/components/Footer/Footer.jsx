49e9c0e8ba4a5c57218590168f766fae88c0c667
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-25 00:18:32 +0530

    Updated footer for link and style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import './Footer.css'
 const Footer = () => {
     return(
         <footer>
-            <p>
-                Created with ❤ by Piyush
-            </p>
-        </footer>
+      <p>
+        Created with 💝 by <a href="https://www.snapit.tech/pgbadgujar007/5w3jtzcp1e">Piyush</a>
+      </p>
+    </footer>
     )
 }
 
